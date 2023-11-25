5359d98eb2e9d868efe7a128b77079298b715d32
Riddhi1444 <rsdani144@gmail.com>
2023-11-25 15:33:38 +0530

    Update number pattern program for readability and maintainability

--- practicals/pattern.php
+++ practicals/pattern.php
@@ -1,13 +1,31 @@
 <?php
-// Specify the number of rows in the inverted triangle
-$rows = 5;
 
-for ($i = $rows; $i >= 1; --$i) {
-    for ($j = 1; $j <= $i; ++$j) {
-        echo "* ";
+/**
+ * Generates an inverted triangle pattern with '*' characters.
+ *
+ * @param int $rows The number of rows in the inverted triangle.
+ * @return void
+ */
+function generateInvertedTriangle($rows) {
+    for ($i = $rows; $i >= 1; --$i) {
+        // Print spaces for indentation
+        for ($j = 1; $j <= $rows - $i; ++$j) {
+            echo "  ";
+        }
+
+        // Print '*' characters for the current row
+        for ($j = 1; $j <= $i; ++$j) {
+            echo "* ";
+        }
+
+        echo PHP_EOL; // Move to the next line
     }
-
-    echo PHP_EOL; // Move to the next line
 }
-?>
 
+// Example usage:
+
+// Specify the number of rows in the inverted triangle
+$numberOfRows = 5;
+
+// Generate and display the inverted triangle
+generateInvertedTriangle($numberOfRows);
