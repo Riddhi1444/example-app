1f20d55817eccd6aaeb893b2d8787c863a4e3ee6
Riddhi1444 <rsdani144@gmail.com>
2023-11-28 00:17:12 +0530

    Update songs view to read from database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Practicals\Song;
+use App\Models\Song;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,30 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/veggies/{sabji}', function (string $sabji) {
-    return $sabji;
-})->whereIn('sabji', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
-
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
-
 Route::get('/veggies', function () {
     return view('veggies');
 });
 
+Route::get('/veggies/{veggieName}', function (string $veggieName) {
+    return $veggieName;
+})->whereIn('veggieName', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
+
 Route::get('/songs', function () {
-    $song1 = new Song();
-    $song1->setTitle("Stan");
-    $song1->setArtist("Eminem");
-  
-    $song2 = new Song();
-    $song2->setTitle("Nothing Else Matters");
-    $song2->setArtist("Metallica");
-  
-    $song3 = new Song();
-    $song3->setTitle("With You");
-    $song3->setArtist("A P Dhillon");
-  
-    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
-  });
+  return view('songs', [ 'songs' => Song::all() ]);
+});
