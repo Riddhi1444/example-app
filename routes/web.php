e85dd1b0adf58a7fdb932e85117eafa355ea6e49
Riddhi1444 <rsdani144@gmail.com>
2023-11-28 01:31:05 +0530

    Added External css file to the components

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// use Practicals/Song;
+// use Practicals\Song;
 use App\Models\Song;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/veggies/{sabji}', function (string $sabji) {
+Route::get('/veggies/{sabji}', function ($sabji) {
+   
     return $sabji;
 })->whereIn('sabji', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
 
 Route::get('/greeting', function () {
     return 'Hello World';
 });
-
-Route::get('/veggies', function () {
+Route::get('/hello', function () {
     return view('veggies');
 });
 
+// On route '/veggies 'a chart should be seen , with name and price of veggies
+Route::get('/veggies', function () {
+    return view('table');
+});
 Route::get('/songs_static', function () {
-    // $song1 = new Song();
+    
+  
+    return view('songs_static'); 
+  });
+
+
+  Route::get('/songs', function () {
+// $song1 = new Song();
     // $song1->setTitle("Stan");
     // $song1->setArtist("Eminem");
   
@@ -43,23 +45,5 @@ Route::get('/songs_static', function () {
     // $song3 = new Song();
     // $song3->setTitle("With You");
     // $song3->setArtist("A P Dhillon");
-  
-    // return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
-    return view('songs_static');
-});
-
-Route::get('/songs', function () {
-    $song1 = new Song();
-    $song1->setTitle("Stan");
-    $song1->setArtist("Eminem");
-  
-    $song2 = new Song();
-    $song2->setTitle("Nothing Else Matters");
-    $song2->setArtist("Metallica");
-  
-    $song3 = new Song();
-    $song3->setTitle("With You");
-    $song3->setArtist("A P Dhillon");
-  
-    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+    return view('songs', [ 'songs' => Song::all() ] );
 });
